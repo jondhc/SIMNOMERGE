49fc7bda4ca1a78bba7452f874b612afcb4a928f
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-10-22 16:16:53 -0500

    Dictionary created.

--- venv/Main.py
+++ venv/Main.py
@@ -1,11 +1,12 @@
 import os
-import lxml.etree as ET
+import lxml.etree as et
 import nltk
 from nltk.tokenize import word_tokenize
 
 paths = []
 trees = []
 ctdf_dictionary = {}
+
 
 def open_docs(directory):
     for docName in os.listdir(directory):
@@ -14,27 +15,32 @@ def open_docs(directory):
             tree = parse(path)
             trees.append(tree)
 
+
 def parse(path):
-    return ET.parse(path)
+    return et.parse(path)
+
 
 def get_leaves_contexts(tree):
-    docName = "test" #TEMPORARY VAR FOR DOC NAME
+    doc_name = "test"  # TEMPORARY VAR FOR DOC NAME
     for element in tree.iter():
         if not element:
             paths.append(tree.getpath(element))
             terms = tokenize_leave(element.text)
             for term in terms:
-                if len(term)>0 and term!="." and term!=",":
-                    store_in_ctdf_dictionary(tree.getpath(element), term, docName)
+                if len(term) > 0 and term != "." and term != ",":
+                    store_in_ctdf_dictionary(tree.getpath(element), term, doc_name)
+
 
 def get_all_leaves_contexts():
     for tree in trees:
         get_leaves_contexts(tree)
 
+
 def tokenize_leave(leave_content):
     text = str(leave_content)
     tokens_list = word_tokenize(text)
     return tokens_list
+
 
 def store_in_ctdf_dictionary(context, term, document):
     context = context + "/" + term
@@ -45,5 +51,3 @@ if __name__ == '__main__':
     open_docs("./Collection/")
     get_all_leaves_contexts()
     print(ctdf_dictionary)
-
-
